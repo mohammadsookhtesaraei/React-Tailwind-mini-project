c5cfbd1d8569b78b889d0773815bfb9d1b6964b4
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2023-09-13 22:08:01 +0330

    create html structure

--- drop-down/src/App.jsx
+++ drop-down/src/App.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React from "react";
 
 const App = () => {
   return (
-    <div>
-      
+    <div className="">
+      <div className="">
+        <button className="">
+          <span>show more</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+            />
+          </svg>
+        </button>
+        <button className=""></button>
+        <div className="">
+          <a className="" href="#">Account</a>
+          <a className="" href="#">Support</a>
+          <a className="" href="#">Setting</a>
+
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
