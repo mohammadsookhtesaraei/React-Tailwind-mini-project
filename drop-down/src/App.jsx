3d8f7f4c956fc1b5357eb9f2852764e4270a9e69
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2023-09-13 22:42:26 +0330

    set tailwind style and usestate cod and End.

--- drop-down/src/App.jsx
+++ drop-down/src/App.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+  
+  // set boolean state
+  const [show,setShow]=useState(false);
+
   return (
-    <div className="">
-      <div className="">
-        <button className="">
+    // parent div
+    <div className="bg-gray-100 flex items-center justify-center h-screen" >
+      {/* second div */}
+      <div className="relative">
+        {/* first button */}
+        <button onClick={()=> setShow(!show)} className={show ? "bg-blue-500 relative z-10  text-white px-4 py-2 rounded-lg inline-flex items-center focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-white" : "bg-blue-500 relative z-10 text-white px-4 py-2 rounded-lg inline-flex items-center "} >
           <span>show more</span>
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -12,7 +19,7 @@ const App = () => {
             viewBox="0 0 24 24"
             stroke-width="1.5"
             stroke="currentColor"
-            class="w-6 h-6"
+            class="w-4 h-4"
           >
             <path
               stroke-linecap="round"
@@ -21,12 +28,14 @@ const App = () => {
             />
           </svg>
         </button>
-        <button className=""></button>
-        <div className="">
-          <a className="" href="#">Account</a>
-          <a className="" href="#">Support</a>
-          <a className="" href="#">Setting</a>
+        {/* second button */}
+        <button onClick={()=> setShow(false)} className={show ? "fixed inset-0 bg-gray-300" : ""}></button>
 
+        {/* third div */}
+        <div className={show ? " bg-gray-100 w-56 overflow-hidden rounded-lg mt-2 absolute" : "hidden"}>
+          <a className="block p-2 hover:bg-blue-400 hover:text-white transition duration-300 ease-in " href="#">Account</a>
+          <a className="block p-2 hover:bg-blue-400 hover:text-white transition duration-300 ease-in" href="#">Support</a>
+          <a className="block p-2 hover:bg-blue-400 hover:text-white transition duration-300 ease-in" href="#">Setting</a>
         </div>
       </div>
     </div>
