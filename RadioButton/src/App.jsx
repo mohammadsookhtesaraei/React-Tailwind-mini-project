33f4c82262186247ceca1df49ea9c5aa4f527755
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2023-10-03 21:42:32 +0330

    set useReducer

--- RadioButton/src/App.jsx
+++ RadioButton/src/App.jsx
@@ -1,8 +1,45 @@
-import React from 'react';
+import React ,{useReducer} from 'react';
+
+const initialState = {
+  first:true,
+  second:true,
+  third:true,
+}
+
+const reducer = (state,action) => {
+  switch(action.type) {
+    case "STARTUP" : 
+    return {
+      first:false,
+      second:true,
+      third:true
+    }
+
+    case "Business" :
+      return {
+        first:true,
+        second:false,
+        third:true
+      }
+
+      case  "ENTERPRISE" :
+        return {
+          first:true,
+          second:true,
+          third:false,
+        }
+
+        default : return state;
+  
+  }
+}
+
 
 const App = () => {
+
+  const [show,dispatch] =useReducer(reducer,initialState);
   return (
-    <div>
+    <div className='bg-blue-600 flex items-center justify-center h-screen'>
       
     </div>
   );
